186eb1829e28b90a9ae76909e9d558ba3a686de0
TLamT <142437528+TLamT@users.noreply.github.com>
2024-10-10 14:34:46 +0800

    Add files via upload

---
+++ mtr.js
@@ -0,0 +1,271 @@
+const line = {
+  KTL: {
+    text: "觀塘線",
+    color: "#7ed957",
+    sta: [
+        { code: "WHA", name: "黃埔" },
+        { code: "HOM", name: "何文田" },
+        { code: "YMT", name: "油麻地" },
+        { code: "MOK", name: "旺角" },
+        { code: "PRE", name: "太子" },
+        { code: "SKM", name: "石硤尾" },
+        { code: "KOT", name: "九龍塘" },
+        { code: "LOF", name: "樂富" },
+        { code: "WTS", name: "黃大仙" },
+        { code: "DIH", name: "鑽石山" },
+        { code: "CHH", name: "彩虹" },
+        { code: "KOB", name: "九龍灣" },
+        { code: "NTK", name: "牛頭角" },
+        { code: "KWT", name: "觀塘" },
+        { code: "LAT", name: "藍田" },
+        { code: "YAT", name: "油塘" },
+        { code: "TIK", name: "調景嶺" },
+    ],
+},
+ISL: {
+    text: "港島線",
+    color: "#004aad",
+    sta: [
+        { code: "KET", name: "堅尼地城" },
+        { code: "HKU", name: "香港大學" },
+        { code: "SYP", name: "西營盤" },
+        { code: "SHW", name: "上環" },
+        { code: "CEN", name: "中環" },
+        { code: "ADM", name: "金鐘" },
+        { code: "WAC", name: "灣仔" },
+        { code: "CAB", name: "銅鑼灣" },
+        { code: "TIH", name: "天后" },
+        { code: "FOH", name: "炮台山" },
+        { code: "NOP", name: "北角" },
+        { code: "QUB", name: "鰂魚涌" },
+        { code: "TAK", name: "太古" },
+        { code: "SWH", name: "西灣河" },
+        { code: "SKW", name: "小蠔灣" },
+        { code: "HFC", name: "杏花邨" },
+        { code: "CHW", name: "柴灣" },
+    ],
+},
+TWL: {
+    text: "荃灣線",
+    color: "#ff3131",
+    sta: [
+        { code: "CEN", name: "中環" },
+        { code: "ADM", name: "金鐘" },
+        { code: "TST", name: "尖沙咀" },
+        { code: "JOR", name: "佐敦" },
+        { code: "YMT", name: "油麻地" },
+        { code: "MOK", name: "旺角" },
+        { code: "PRE", name: "太子" },
+        { code: "SSP", name: "深水埗" },
+        { code: "CSW", name: "長沙灣" },
+        { code: "LCK", name: "荔枝角" },
+        { code: "MEF", name: "美孚" },
+        { code: "LAK", name: "荔景" },
+        { code: "KWF", name: "葵芳" },
+        { code: "KWH", name: "葵興" },
+        { code: "TWH", name: "大窩口" },
+        { code: "TSW", name: "荃灣" },
+    ],
+},
+SIL: {
+    text: "南港島線",
+    color: "#cbcd09",
+    sta: [
+        { code: "ADM", name: "金鐘" },
+        { code: "OCP", name: "海洋公園" },
+        { code: "WCH", name: "黃竹坑" },
+        { code: "LET", name: "利東" },
+        { code: "SOH", name: "南灣" },
+    ],
+},
+TCL: {
+    text: "東涌線",
+    color: "#f6943d",
+    sta: [
+        { code: "HOK", name: "香港" },
+        { code: "KOW", name: "九龍" },
+        { code: "OLY", name: "奧運" },
+        { code: "NAC", name: "南昌" },
+        { code: "LAK", name: "荔景" },
+        { code: "TSY", name: "青衣" },
+        { code: "SUN", name: "陽明山" },
+        { code: "TUC", name: "東涌" },
+    ],
+},
+EAL: {
+    text: "東鐵線",
+    color: "#5ce1e6",
+    sta: [
+        { code: "ADM", name: "金鐘" },
+        { code: "EXC", name: "會展" },
+        { code: "HUH", name: "紅磡" },
+        { code: "MKK", name: "旺角東" },
+        { code: "KOT", name: "九龍塘" },
+        { code: "TAW", name: "大圍" },
+        { code: "SHT", name: "沙田" },
+        { code: "FOT", name: "火炭" },
+        { code: "RAC", name: "賽馬會" },
+        { code: "UNI", name: "大學" },
+        { code: "TAP", name: "大埔墟" },
+        { code: "TWO", name: "太和" },
+        { code: "FAN", name: "粉嶺" },
+        { code: "SHS", name: "上水" },
+        { code: "LOW", name: "羅湖" },
+        { code: "LMC", name: "落馬洲" },
+    ],
+},
+TML: {
+    text: "屯馬線",
+    color: "#8d6019",
+    sta: [
+        { code: "WKS", name: "烏溪沙" },
+        { code: "MOS", name: "馬鞍山" },
+        { code: "HEO", name: "恒安" },
+        { code: "TSH", name: "大水坑" },
+        { code: "SHM", name: "石門" },
+        { code: "CIO", name: "城市一號" },
+        { code: "STW", name: "沙田圍" },
+        { code: "CKT", name: "車公廟" },
+        { code: "TAW", name: "大圍" },
+        { code: "HIK", name: "欣景" },
+        { code: "DIH", name: "鑽石山" },
+        { code: "KAT", name: "啟德" },
+        { code: "SUW", name: "頌恩" },
+        { code: "TKW", name: "土瓜灣" },
+        { code: "HOM", name: "何文田" },
+        { code: "HUH", name: "紅磡" },
+        { code: "ETS", name: "東尖沙咀" },
+        { code: "AUS", name: "柯士甸" },
+        { code: "NAC", name: "南昌" },
+        { code: "MEF", name: "美孚" },
+        { code: "TWW", name: "荃灣西" },
+        { code: "KSR", name: "錦上路" },
+        { code: "YUL", name: "元朗" },
+        { code: "LOP", name: "朗屏" },
+        { code: "TIS", name: "天水圍" },
+        { code: "SIH", name: "兆康" },
+        { code: "TUM", name: "屯門" },
+    ],
+},
+AEL: {
+    text: "機場快線",
+    color: "#3d9ea0",
+    sta: [
+        { code: "HOK", name: "香港" },
+        { code: "KOW", name: "九龍" },
+        { code: "TSY", name: "青衣" },
+        { code: "AIR", name: "機場" },
+        { code: "AWE", name: "亞洲國際博覽館" },
+    ],
+},
+TKL: {
+    text: "將軍澳線",
+    color: "#8d45ab",
+    sta: [
+        { code: "NOP", name: "北角" },
+        { code: "QUB", name: "鰂魚涌" },
+        { code: "YAT", name: "油塘" },
+        { code: "TIK", name: "調景嶺" },
+        { code: "TKO", name: "將軍澳" },
+        { code: "HAH", name: "坑口" },
+        { code: "POA", name: "寶琳" },
+        { code: "LHP", name: "康城" },
+    ],
+},
+};
+let api="https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php"
+document.addEventListener("DOMContentLoaded", function () {
+  renderBtn();
+  console.log(line);
+});
+
+async function updatedTime(){
+  const lastUpdateTime = document.querySelector("#time")
+  const currentTime = document.createElement("h2")
+  const response = await fetch(api)
+  const result = await response.json()
+  lastUpdateTime.innerHTML= `最後更新時間: ${result.timestamp.split(" ")[1].substring(0,5)}`
+
+
+  
+}
+function renderBtn() {
+  const btnContainer = document.getElementById("container");
+  for (let key in line) {
+    const btn = document.createElement("div");
+    btn.className = "line-button";
+    btn.innerHTML = line[key].text;
+    btn.style.color = line[key].color;
+    btn.style.borderColor = line[key].color;
+    btn.id = key;
+
+    btn.addEventListener("click", function () {
+      
+      resetBtnColor();
+      updatedTime()
+      btn.style.backgroundColor = line[key].color;
+      btn.style.color = "#ffffff";
+      fetchMTRData(key);
+    });
+    btnContainer.appendChild(btn);
+  }
+}
+function resetBtnColor() {
+  const btnArr = document.getElementsByClassName("line-button");
+  for (let i = 0; i < btnArr.length; i++) {
+    btnArr[i].style.color = line[btnArr[i].id].color;
+    btnArr[i].style.backgroundColor = "#ffffff";
+  }
+  
+}
+async function fetchMTRData(lineKey) {
+  lineArr = line[lineKey].sta;
+  const upCard = document.querySelector("#up-line");
+  const downCard = document.querySelector("#down-line");
+  upCard.innerHTML=""
+  downCard.innerHTML=""
+  lineArr.map(async (station) => {
+    const code = station.code;
+    const response = await fetch(
+      `${api}?line=${lineKey}&sta=${code}`
+    );
+    const result = await response.json();
+    console.log(result);
+    const info = result.data[`${lineKey}-${code}`];
+    if (info.UP && info.UP[0]) {
+      const stationInfo = info.UP[0];
+      
+      up = renderCard(line[lineKey].color, station.name, stationInfo);
+      upCard.appendChild(up);
+    }
+    if (info.DOWN && info.DOWN[0]) {
+      const stationInfo = info.DOWN[0];
+      down = renderCard(line[lineKey].color, station.name, stationInfo);
+      downCard.appendChild(down);
+    }
+  });
+}
+function renderCard(cardColor, stationName, stationInfo) {
+  const card = document.createElement("div");
+  card.className = "station";
+  card.style.backgroundColor = cardColor;
+  
+  const cardTitle = document.createElement("h3");
+  cardTitle.className = "station-name";
+  cardTitle.textContent = stationName;
+  card.appendChild(cardTitle);
+  
+  const cardInfo = document.createElement("div");
+  cardInfo.className = "station-Info";
+  card.appendChild(cardInfo);
+  
+  const cardTime = document.createElement("div");
+  cardTime.className = "station-next";
+  cardTime.textContent = `下班列車: ${stationInfo.time
+    .split(" ")[1]
+    .substring(0, 5)}`;
+    cardInfo.appendChild(cardTime);
+  return card
+}
+
+
